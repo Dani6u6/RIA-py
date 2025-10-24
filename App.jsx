fa8821a4ec9f9c41fc7aeeb8c5c003830aaa93e7
Dani Osuna <dani.dv6u6@gmail.com>
2025-10-23 20:13:40 -0700

    Se agregó un logo premilimar al header y se corrigió el nombre de appScript.jsx por appScripts.jsx

--- App.jsx
+++ App.jsx
@@ -96,7 +96,8 @@ export default function App() {
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
               <div className="w-10 h-10 bg-gradient-to-br from-blue-500 to-purple-600 rounded-lg flex items-center justify-center">
-                <Sparkles className="w-6 h-6 text-white" />
+                {/* <Sparkles className="w-6 h-6 text-white" /> */}
+                <img src="/components/img/logoRIA.svg" alt="Logo" className="w-6 h-6" />
               </div>
               <div>
                 <h1 className="text-gray-900 dark:text-white">rIA</h1>
